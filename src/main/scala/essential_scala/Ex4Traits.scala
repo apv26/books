5c489f7800fa8720970d4ffd7717b0cfc5eda0f9
alex <alex@eldis.ru>
2019-09-18 17:23:36 +0700

    traits, email, trees

--- src/main/scala/essential_scala/Ex4Traits.scala
+++ src/main/scala/essential_scala/Ex4Traits.scala
@@ -1,5 +1,7 @@
 package essential_scala
 
+import java.util.Date
+
 object Ex4Traits extends App {
 
   //  4.1.4.1 Cats,and MoreCats
@@ -56,4 +58,100 @@ object Ex4Traits extends App {
   }
 
   //  4.5.6.3 Email
+  trait Visitor {
+    def id: String
+    def createdAt: Date
+    def age: Long = new Date().getTime - createdAt.getTime
+  }
+  case class Anonymous(
+    id: String,
+    createdAt: Date = new Date()
+  ) extends Visitor
+  case class User(
+    id: String,
+    email: String,
+    createdAt: Date = new Date()
+  ) extends Visitor
+  // not only depends on other fields and methods in a class
+  //  we generally prefer sealed traits
+  object EmailService {
+    def email(visitor: Visitor): Unit = {
+      visitor match {
+        case a: Anonymous => ()
+        case u: User      => println("send " + u.email)
+      }
+    }
+  }
+
+  // 4.6.3.2 The Forest of Trees
+  // pattern matching
+  sealed trait Tree {}
+  case class Node(left: Tree, right: Tree) extends Tree
+  case class Leaf(element: Int) extends Tree
+  object Math {
+    def prettyPrint(t: Tree, indent: String): Unit = t match {
+      case (t: Node) =>
+        prettyPrint(t.left, indent + " ")
+        prettyPrint(t.right, indent + " ")
+        println(indent + ".")
+      case (t: Leaf) => println(indent + t.element)
+    }
+    def sum(t: Tree): Int = t match {
+      case (t: Node) => sum(t.left) + sum(t.right)
+      case (t: Leaf) => t.element
+    }
+    def double(t: Tree): Tree = t match {
+      case (t: Node) => Node(double(t.left), double(t.right))
+      case (t: Leaf) => Node(Leaf(t.element), Leaf(t.element))
+    }
+  }
+  val t =
+    Node(
+      Node(
+        Node(
+          Leaf(5),
+          Leaf(4)
+        ),
+        Leaf(3)
+      ),
+      Leaf(2)
+    )
+  Math.prettyPrint(t, "")
+  println(Math.sum(t))
+  Math.prettyPrint(Math.double(t), "")
+
+  // using polymorphism
+  sealed trait Tree1 {
+    def prettyPrint(indent: String): Unit
+    def sum(): Int
+    def double(): Tree1
+  }
+  case class Node1(left: Tree1, right: Tree1) extends Tree1 {
+    def prettyPrint(indent: String): Unit = {
+      left.prettyPrint(indent + " ")
+      right.prettyPrint(indent + " ")
+      println(indent + ".")
+    }
+    def sum(): Int = left.sum() + right.sum()
+    def double(): Tree1 = Node1(left.double(), right.double())
+  }
+  case class Leaf1(element: Int) extends Tree1 {
+    def prettyPrint(indent: String): Unit = println(indent + element)
+    def sum(): Int = element
+    def double(): Tree1 = Node1(Leaf1(element), Leaf1(element))
+  }
+  val t1 =
+    Node1(
+      Node1(
+        Node1(
+          Leaf1(5),
+          Leaf1(4)
+        ),
+        Leaf1(3)
+      ),
+      Leaf1(2)
+    )
+  t1.prettyPrint("")
+  println(t1.sum())
+  t1.double().prettyPrint("")
 }
